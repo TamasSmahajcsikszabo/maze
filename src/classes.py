8fbbcfb9940c81fe49d448d7b9bee745fc29f358
tamas <tamas.smahajcsikszabo@outlook.hu>
2021-02-22 22:53:22 +0100

    custom cell size

--- src/classes.py
+++ src/classes.py
@@ -61,31 +61,6 @@ class Room(Component):
         self.body = "   "
 
 
-def get_base_components():
-    NE_corner = Corner("NE", indicator=2, name="NE_corner")
-    NW_corner = Corner("NW", indicator=0, name="NW_corner")
-    N_wall = Wall(orientation="N", indicator=1, name="N_wall")
-    SE_corner = Corner("SE", indicator=8, name="SE_corner")
-    SW_corner = Corner("SW", indicator=6, name="SW_corner")
-    S_wall = Wall(orientation="S", indicator=7, name="S_wall")
-    room = Room(indicator=4, name="room")
-    W_wall = Wall(orientation="W", indicator=3, name="W_wall")
-    E_wall = Wall(orientation="E", indicator=5, name="E_wall")
-
-    components = [
-        NW_corner,
-        N_wall,
-        NE_corner,
-        E_wall,
-        room,
-        W_wall,
-        SE_corner,
-        S_wall,
-        SW_corner]
-
-    return components
-
-
 def get_matrix(x=10, y=10):
     cells = list(range(x * y))
     matrix = list()
@@ -99,36 +74,130 @@ def get_matrix(x=10, y=10):
     return matrix
 
 
+def find_unbroken_corridors(matrix, algorithm="BT"):
+    if algorithm == "BT":
+        northern_corridor = [e for e in matrix[0]]
+        eastern_corridor = [max(r) for r in matrix]
+        return (northern_corridor, eastern_corridor)
+
+    if algorithm == "sidewinder":
+        norhern_corridor = matrix[0]
+        return norhern_corridor
+
+
+def find_boundaries(matrix, combined=True, algorithm="BT"):
+    if algorithm == "BT":
+        corners = get_corners(matrix)
+        northern_corridor = [i for i in matrix[0] if i not in corners]
+        southern_corridor = [
+            i for i in matrix[len(matrix)-1] if i not in corners]
+        eastern_corridor = [max(r) for r in matrix if max(r) not in corners]
+        western_corridor = [min(r) for r in matrix if min(r) not in corners]
+        horizontal = []
+        [horizontal.extend(l) for l in (northern_corridor, southern_corridor)]
+        vertical = []
+        [vertical.extend(l) for l in (eastern_corridor, western_corridor)]
+        if combined:
+            return (horizontal, vertical)
+        else:
+            return [northern_corridor, southern_corridor,
+                    eastern_corridor, western_corridor]
+
+def get_base_components(size=3):
+    cellmatrix = get_matrix(size, size)
+    corners = [i for i in get_corners(cellmatrix)]
+
+    NW_corner = Corner("NW", indicator=corners[0], name="NW_corner")
+    NE_corner = Corner("NE", indicator=corners[1], name="NE_corner")
+    SW_corner = Corner("SW", indicator=corners[2], name="SW_corner")
+    SE_corner = Corner("SE", indicator=corners[3], name="SE_corner")
+    north_wall, south_wall, east_wall, west_wall = find_boundaries(
+        cellmatrix, combined=False)
+    occupied_space = []
+    for container in [corners, north_wall, south_wall, east_wall, west_wall]:
+        occupied_space.extend(container)
+    room_spaces = [i for i in range(size*size) if i not in occupied_space]
+
+    N_walls = [Wall(orientation="N", indicator=i, name="N_wall")
+               for i in north_wall]
+    S_walls = [Wall(orientation="S", indicator=i, name="S_wall")
+               for i in south_wall]
+    rooms = [Room(indicator=i, name="room") for i in room_spaces]
+    W_walls = [Wall(orientation="W", indicator=i, name="W_wall")
+               for i in west_wall]
+    E_walls = [Wall(orientation="E", indicator=i, name="E_wall")
+               for i in east_wall]
+
+    components = [
+        NW_corner,
+        N_walls,
+        NE_corner,
+        E_walls,
+        rooms,
+        W_walls,
+        SE_corner,
+        S_walls,
+        SW_corner]
+
+    return components
+
+def get_middle_value(x):
+    if x % 2 == 1:
+        for i in range(x):
+            first = list(range(0, i))
+            second = list(range(i+1, x))
+            if len(first) == len(second):
+                return i
+    else:
+        result  = get_middle_value(x-1)
+        return [result, result+1]
+
 def open_wall(orientation, cells):
     # first cell is either northern or western
+    wall_length = cells[0].wall_length
     if len(cells) == 2:
+        if wall_length % 2 == 1:
+            middle = str(get_middle_value(wall_length))
 
-        if orientation == "N":
-            cells[0].S_wall.body = "╕ ╒"
-            cells[1].N_wall.body = "╛ ╘"
+            if orientation == "N":
 
-        if orientation == "E":
-            cells[0].E_wall.body = " "
-            cells[1].W_wall.body = " "
+                getattr(cells[0], "S_wall"+middle).body = "╕ ╒"
+                getattr(cells[1], "N_wall"+middle).body = "╛ ╘"
+
+            if orientation == "E":
+                getattr(cells[0], "E_wall"+middle).body = " "
+                getattr(cells[1], "W_wall"+middle).body = " "
 
     else:
         pass
 
-
 class Cell(Component):
-    def __init__(self, **kargs):
+    def __init__(self, size=3, **kargs):
         Component.__init__(self, **kargs)
-        components = get_base_components()
-        self.components = components
+        components = get_base_components(size=size)
+        self.size = size
+        self.components = []
+        self.matrix = get_matrix(x=size, y=size)
+        self.wall_length = self.size - 2
         for component in components:
-            setattr(self, component.name, component)
+            if isinstance(component, list):
+                for i in range(len(component)):
+                    if component[i].__class__.__name__ == "Room":
+                        setattr(self, component[i].name + str(i), component[i])
+                        self.components.append(component[i])
+                    elif component[i].__class__.__name__ == "Wall":
+                        setattr(self, component[i].name + str(i), component[i])
+                        self.components.append(component[i])
+            else:
+                setattr(self, component.name, component)
+                self.components.append(component)
 
-        for i in [3, 6, 9]:
+        for i in [max(m)+1 for m in self.matrix]:
             section = []
-            if i == 3:
+            if i == size:
                 r = range(i)
             else:
-                r = range(i-3, i)
+                r = range(i-size, i)
             for y in r:
                 for component in self.components:
                     if y == component.indicator:
@@ -137,10 +206,10 @@ class Cell(Component):
 
     def __repr__(self):
         cellstring = ""
-        for i in range(9):
+        for i in range(self.size*self.size):
             for component in self.components:
                 if i == component.indicator:
-                    if i in [2, 5, 8]:
+                    if i in [max(e) for e in self.matrix]:
                         cellstring = cellstring + component.body + "\n"
                     else:
                         cellstring = cellstring + component.body
@@ -149,7 +218,6 @@ class Cell(Component):
 
     def __getitem__(self, section):
         return getattr(self, section)
-
 
 def merge_objects(section):
     string = ""
@@ -165,35 +233,6 @@ def get_corners(m):
     SW = min(m[len(m)-1])
     SE = max(m[len(m)-1])
     return (NW, NE, SW, SE)
-
-
-def find_unbroken_corridors(matrix, algorithm="BT"):
-    if algorithm == "BT":
-        northern_corridor = [e for e in matrix[0]]
-        eastern_corridor = [max(r) for r in matrix]
-        return (northern_corridor, eastern_corridor)
-
-    if algorithm == "sidewinder":
-        norhern_corridor = matrix[0]
-        return norhern_corridor
-
-
-def find_boundaries(matrix,combined=True, algorithm="BT"):
-    if algorithm == "BT":
-        corners = get_corners(matrix)
-        northern_corridor = [i for i in matrix[0] if i not in corners]
-        southern_corridor = [
-            i for i in matrix[len(matrix)-1] if i not in corners]
-        eastern_corridor = [max(r) for r in matrix if max(r) not in corners]
-        western_corridor = [min(r) for r in matrix if min(r) not in corners]
-        horizontal = []
-        [horizontal.extend(l) for l in (northern_corridor, southern_corridor)]
-        vertical = []
-        [vertical.extend(l) for l in (eastern_corridor, western_corridor)]
-        if combined:
-            return (horizontal, vertical)
-        else:
-            return [northern_corridor, southern_corridor, eastern_corridor, western_corridor]
 
 
 def collapse_to_list(tupl):
@@ -257,7 +296,8 @@ def generate_maze(selfObject):
                 selected_indicator = selfObject.matrix[row][c]
                 for cell in selfObject.cells:
                     if cell.indicator == selected_indicator:
-                        if cell.indicator in find_boundaries(selfObject.matrix,combined=False)[2] or cell.indicator == selfObject.corners[3]:
+                        if cell.indicator in find_boundaries(selfObject.matrix, combined=False)[
+                                                             2] or cell.indicator == selfObject.corners[3]:
                             other_cell = [
                                 cell for cell in selfObject.cells
                                 if cell.indicator == selfObject.matrix
@@ -298,13 +338,14 @@ def generate_maze(selfObject):
 
 
 class Maze(Component):
-    def __init__(self, x, y, algorithm="BT",  **kargs):
+    def __init__(self, x, y, cellsize=4, algorithm="BT",  **kargs):
         Component.__init__(self, **kargs)
         self.x = x
         self.y = y
+        self.cellsize = cellsize
         self.matrix = get_matrix(x, y)
         self.cells = [
-            Cell(indicator=i, name="")
+            Cell(indicator=i, size = self.cellsize, name="")
             for i in [row[i] for row in self.matrix for i in range(
                          len(row))]]
         self.algorithm = algorithm
@@ -322,7 +363,7 @@ class Maze(Component):
     def __repr__(self):
         maze_string = ""
         for row in self.matrix:
-            for section in ["section_3", "section_6", "section_9"]:
+            for section in ['section_'+str(max(m)+1) for m in get_matrix(x=self.cellsize, y=self.cellsize)]:
                 for i in row:
                     for selected_cell in self.cells:
                         if selected_cell.indicator == i:
@@ -332,22 +373,17 @@ class Maze(Component):
         return maze_string
 
 
-#quick checks
-test_maze = Maze(x=14, y=10, indicator=1, name="test_maze")
-matrix = get_matrix()
-get_corners(matrix)
-test_maze.matrix
-test_maze.unbroken_corridors[0]
-test_maze.corners
-test_maze.boundaries
+# quick checks
+test_maze = Maze(x=14, y=10, cellsize=3, indicator=1, name="test_maze")
 test_maze2 = Maze(
     x=14,
     y=10,
+    cellsize=3,
     algorithm="sidewinder",
     indicator=2,
     name="test2")
 
-#TODO
+# TODO
 # free isolated cells
 # change cell size
 # add items
